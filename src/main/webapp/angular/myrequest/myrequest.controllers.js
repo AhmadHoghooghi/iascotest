1d4fde2003ece29008234b62561abf749f5e4789
Ahmad Hoghooghi <ahmad.hoghooghi@gmail.com>
2019-07-21 11:11:36 +0430

    Implement Requirement

--- src/main/webapp/angular/myrequest/myrequest.controllers.js
+++ src/main/webapp/angular/myrequest/myrequest.controllers.js
@@ -1,34 +1,51 @@
 angular.module('iascotest.myrequest')
     .controller('MyrequestEditCtrl', ['$scope', 'MyrequestResource', 'iascotestControllerService', function ($scope, MyrequestResource, iascotestControllerService) {
-      var loading = $.fn.showLoading();
-    	iascotestControllerService.init($scope);
-     	iascotestControllerService.applyEditCtrl($scope,'myRequest', MyrequestResource, function (data) {
-        $.fn.hideLoading(loading);
-      });
+        var loading = $.fn.showLoading();
+        iascotestControllerService.init($scope);
+        iascotestControllerService.applyEditCtrl($scope, 'myRequest', MyrequestResource, function (data) {
+            $.fn.hideLoading(loading);
+        });
+    }])
+    .controller('MyrequestListCtrl', ['$scope', 'MyrequestResource', 'iascotestControllerService','dialogs',
+                            function ($scope, MyrequestResource, iascotestControllerService, dialogs) {
+        var loading = $.fn.showLoading();
+        iascotestControllerService.init($scope);
+        iascotestControllerService.applyListCtrl($scope, 'myRequest', MyrequestResource, function (data) {
+            $.fn.hideLoading(loading);
+        });
+        $scope.manageResponsibles = function(masterpk){
+            dialogs.create('/iascotest/angular/myrequest/myrequest_editresponsible.html', 'SetResponsibleForRequestCtrl', {masterpk:masterpk});
+        };
+
+    }])
+    .controller('MyrequestDisplayCtrl', ['$scope', '$location', '$routeParams', 'MyrequestResource', 'iascotestControllerService'
+        , function ($scope, $location, $routeParams, MyrequestResource, iascotestControllerService) {
+        var loading = $.fn.showLoading();
+        iascotestControllerService.init($scope);
+        iascotestControllerService.applyDisplayCtrl($scope, 'myRequest', MyrequestResource, function (data) {
+            $.fn.hideLoading(loading);
+        });
+    }])
+    //todo something else should be injected insted of 'MyrequestResource'; or??
+    .controller('SetResponsibleForRequestCtrl', ['$scope', 'MyrequestResource', 'iascotestControllerService', 'data', '$modalInstance',
+        function ($scope, MyrequestResource, iascotestControllerService, data, modalInstance) {
+        // var loading = $.fn.showLoading();
+        iascotestControllerService.init($scope);
+
+        iascotestControllerService.applyMinimalCtrl($scope, 'myRequest', MyrequestResource);
+        //applyMinimalCtrl = function ($scope, entityName, remoteResource)
+        //     $.fn.hideLoading(loading);
+        $scope.myresponsibleParams = {
+            hideSearchPanel: true,
+            masterPK: $scope.id,
+            //
+            myrequestId:data.masterpk,
+            'filter.eq:request.pk': $scope.id
+        };
+        $scope.back = function () {
+            modalInstance.dismiss();
+        }
+
     }])
 
-    .controller('MyrequestListCtrl', ['$scope', 'MyrequestResource', 'iascotestControllerService', function ($scope, MyrequestResource, iascotestControllerService) {
-      var loading = $.fn.showLoading();
-    	iascotestControllerService.init($scope);
-     	iascotestControllerService.applyListCtrl($scope,'myRequest', MyrequestResource, function (data) {
-        $.fn.hideLoading(loading);
-      });
-    }])
-    
-    	
-
-    .controller('MyrequestDisplayCtrl', ['$scope', '$location', '$routeParams', 'MyrequestResource', 'iascotestControllerService', function ($scope, $location, $routeParams, MyrequestResource, iascotestControllerService) {
-      var loading = $.fn.showLoading();
-    	iascotestControllerService.init($scope);
-      iascotestControllerService.applyDisplayCtrl($scope,'myRequest', MyrequestResource, function (data) {
-          $.fn.hideLoading(loading);
-      });
-      
-      
-      
-		$scope.myresponsibleParams={
-      hideSearchPanel: true,
-      masterPK:$scope.id,
-      'filter.eq:request.pk': $scope.id
-    };
-    }]);
+;
--- src/main/webapp/angular/myresponsible/myresponsible.controllers.js
+++ src/main/webapp/angular/myresponsible/myresponsible.controllers.js
@@ -1,21 +1,82 @@
 angular.module('iascotest.myresponsible')
-    .controller('MyresponsibleEditCtrl', ['$scope', 'MyresponsibleResource', 'iascotestControllerService', function ($scope, MyresponsibleResource, iascotestControllerService) {
+    .controller('MyresponsibleEditCtrl', ['$scope', 'MyresponsibleResource', 'iascotestControllerService', 'data', '$modalInstance',
+                function ($scope, MyresponsibleResource, iascotestControllerService, data, modalInstance) {
       var loading = $.fn.showLoading();
     	iascotestControllerService.init($scope);
+
+    	if(data.params && data.params.myrequestId){//if called from SetResponsibleForRequestCtrl
+    	    //overriding $scope.actions.edit.save from lib.actions
+            $scope.actions.edit.save = function (remoteResource, entityName, flash, updateCallback, masterFieldName) {
+                var loading = $.fn.showLoading();
+                    //this is customized line
+                    $scope.data['request'] = {pk: data.params.myrequestId};
+
+                if ($scope.actions.common.validateForm(flash, "#" + entityName + "EditForm")) {
+                    remoteResource.update($scope.data).$promise.then(function (data) {
+                        $.fn.hideLoading(loading);
+                        if (data.success) {
+                            flash.success = data.message;
+                            updateCallback = function () {
+                                modalInstance.close()
+                            }
+
+                            if (angular.isFunction(updateCallback))
+                                updateCallback(data);
+                        } else {
+                            flash.error = data.message;
+                        }
+                    }, function (error) {
+                        $.fn.hideLoading(loading);
+                        flash.error = error.data.message;
+                    });
+                    return true;
+                } else {
+                    $.fn.hideLoading(loading);
+                }
+                return false;
+            };
+        }
+
+
      	iascotestControllerService.applyEditCtrl($scope,'myResponsible', MyresponsibleResource, function (data) {
         $.fn.hideLoading(loading);
       });
+
+    	$scope.back = function () {
+            modalInstance.dismiss();
+        }
     }])
 
-    .controller('MyresponsibleListCtrl', ['$scope', 'MyresponsibleResource', 'iascotestControllerService', function ($scope, MyresponsibleResource, iascotestControllerService) {
+    .controller('MyresponsibleListCtrl', ['$scope', 'MyresponsibleResource', 'iascotestControllerService',
+                                    function ($scope, MyresponsibleResource, iascotestControllerService) {
       var loading = $.fn.showLoading();
-    	iascotestControllerService.init($scope);
+        iascotestControllerService.init($scope);
+
+        $scope.actions.common.makeEditDialog = function (entityName, id, remoteResource, dialogs, updateCallback, master_id) {
+            dialogs.create('/iascotest/angular/myresponsible/myresponsible.edit.html',  'MyresponsibleEditCtrl', {
+                params: {
+                    pk: id,
+                    masterPK: master_id,
+                    myrequestId:$scope.parameters.myrequestId,
+                    ignoreQueryString: true
+                },
+                updateCallback: updateCallback,
+                entityName: entityName,
+                resource: remoteResource
+            }).result.then(function () {
+                $scope.actions.list.reloadTable();
+            });
+        };
+        // initializing search filter using given request id
+        if($scope.parameters && $scope.parameters.myrequestId){//calling list from SetResponsibleForRequestCtrl
+            $scope.parameters['eq:request.pk'] = $scope.parameters.myrequestId;
+        }
+
+
      	iascotestControllerService.applyListCtrl($scope,'myResponsible', MyresponsibleResource, function (data) {
         $.fn.hideLoading(loading);
       });
     }])
-    
-    	
 
     .controller('MyresponsibleDisplayCtrl', ['$scope', '$location', '$routeParams', 'MyresponsibleResource', 'iascotestControllerService', function ($scope, $location, $routeParams, MyresponsibleResource, iascotestControllerService) {
       var loading = $.fn.showLoading();
